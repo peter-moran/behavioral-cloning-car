1a5e7d3fb94b8af2395a3d9bca2a587d889c11c8
Peter Moran <petecmo@gmail.com>
2017-07-24 15:58:30 -0400

    Data augmented with mirror images.

--- model.py
+++ model.py
@@ -9,7 +9,6 @@ from keras import backend as K
 import tensorflow as tf
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
-from random import randint
 
 # Fix tf bug
 K.clear_session()
@@ -49,8 +48,9 @@ def plot_history(losses):
     plt.xlabel('epoch')
     plt.legend(['training set', 'validation set'], loc='upper right')
 
+
 class VirtualSet:
-    def __init__(self, sample_set, batch_size):
+    def __init__(self, sample_set, batch_size, augment=False):
         """
         Acts as an interface to both real sampled data and augmented data for datasets passed through the network
         (ie training set, validation set, etc.)
@@ -59,9 +59,13 @@ class VirtualSet:
         simulation measurements. This will be the raw data for the training, validation, or test set.
         :param batch_size: Number of samples to pass to the network each call to the generator function.
         """
-        self.samples = sample_set
+        self.raw_samples = sample_set
+        self.n_raw_samples = len(self.raw_samples)
         self.batch_size = batch_size
-        self.n_batches = len(self.samples) / self.batch_size
+        self.n_samples = self.n_raw_samples
+        if augment:
+            self.n_samples *= 2
+        self.n_batches = self.n_samples / self.batch_size
 
     def generator_func(self):
         """
@@ -70,30 +74,39 @@ class VirtualSet:
         :return: features and associated labels, ready to be passed to the network.
         """
         while True:
-            shuffle(self.samples)
-            for batch_start in range(0, len(self.samples), self.batch_size):
-                batch = self.samples[batch_start:batch_start + self.batch_size]
-
-                features = np.array([imread(sample['img_center']) for sample in batch])
-                labels = np.array([sample['angle'] for sample in batch])
-
-                yield features, labels
+            arg_shuffle = shuffle(range(self.n_samples))
+            for offset in range(0, self.n_samples, self.batch_size):
+                features = []
+                labels = []
+                for ndx in arg_shuffle[offset:offset+self.batch_size]:
+                    sample = self.raw_samples[ndx % self.n_raw_samples]
+                    image = imread(sample['img_center'])
+                    angle = sample['angle']
+                    if ndx < self.n_raw_samples:
+                        # Use sample as is
+                        features.append(image)
+                        labels.append(angle)
+                    elif ndx >= self.n_raw_samples:
+                        # Augment sample
+                        features.append(np.fliplr(image))
+                        labels.append(-angle)
+                yield np.array(features), np.array(labels)
 
 
 def create_model():
     model = Sequential()
 
-    # Preprocessing
+    # Pre-processing
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-    model.add(Cropping2D(cropping=((70,25),(0,0))))
+    model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 
     # VGG inspired structure
     model.add(Conv2D(64, (5, 5), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(3,3)))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
     model.add(Conv2D(128, (5, 5), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(3,3)))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
     model.add(Conv2D(256, (5, 5), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(3,3)))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
     model.add(Flatten())
     model.add(Dense(512, activation='relu'))
     model.add(Dense(128, activation='relu'))
@@ -109,7 +122,7 @@ if __name__ == '__main__':
     samples_train, samples_validation = train_test_split(simulator_samples, test_size=0.3)
 
     # Set up generators
-    train_set = VirtualSet(samples_train, batch_size=32)
+    train_set = VirtualSet(samples_train, batch_size=32, augment=True)
     train_generator = train_set.generator_func()
     validation_set = VirtualSet(samples_validation, batch_size=32)
     validation_generator = validation_set.generator_func()
@@ -122,10 +135,10 @@ if __name__ == '__main__':
                                  steps_per_epoch=train_set.n_batches,
                                  validation_data=validation_generator,
                                  validation_steps=validation_set.n_batches,
-                                 epochs=10)
+                                 epochs=6)
     model.save('model.h5')
 
     # Plot loss
     plot_history(losses)
-    plt.ylim([0,.005])
+    plt.ylim([0, .005])
     plt.show()
